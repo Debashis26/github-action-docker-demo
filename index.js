1f7579460a57ea846ba30cd32cbac43730d71a5f
Badajena, Debashis [GTSSG NON-J&J] <DBadajen@its.jnj.com>
2024-04-28 14:08:44 +0530

    docker file configured_v1.4

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-var moment=require("moment")
+// app.js
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.get('/docker-test', (req, res) => {
+    console.log("logs for express app");
+  res.send('Hello, world!');
+});
 
-var date= moment().format("DD-MM-YY")
-console.log("date: ",date);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
